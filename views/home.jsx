06acf05469b59f42e8e926002f9bf3e19aad0f01
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-25 21:31:04 -0500

    added main image

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/food.jpg" alt="food image"/>
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@edgarraw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar Castrejon</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
               <a href="/places">
               <button className="btn-primary">Places Page</button>
               </a>
