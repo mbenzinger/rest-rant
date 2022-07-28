6de8a039212ea8c18479c84b64f8e4b26bb045a1
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-27 21:12:45 -0500

    added for control classes

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home () {
           <main>
               <h1>HOME</h1>
               <div>
-                <img src="/images/food.jpg" alt="food image"/>
+                <img id='hero' src="/images/food.jpg" alt="food image"/>
               </div>
               <div>
               Photo by <a href="https://unsplash.com/@edgarraw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edgar Castrejon</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
