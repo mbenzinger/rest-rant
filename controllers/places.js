0c974985ffd14cd53348ba870d9ec80d53f323b8
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-29 19:50:57 -0500

    pasted in more code.  unlikely that this works.

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,12 @@
 const router = require('express').Router()
-
-// GET /places
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
-})
-
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'images/taylor-kiser-6RJct18G_3I-unsplash.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/nathan-dumlao-6VhPY27jdps-unsplash.jpg'
-      }]
-      
-      res.render('places/index', { places })
+    res.render('places/index', { places })
+})
 
-  })
+// More code ...
+
   
   
 
---
+++ models/places.js
@@ -0,0 +1,32 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg'
+}]
+
+
+//I don't know where this code is supposed to go, instructinos say to add to POST / Places route.
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
