aa166a672106f8130b6e7e455ac3a3bdfa1436d8
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-30 14:53:14 -0500

    got the places controller working properly, added route for new

--- controllers/places.js
+++ controllers/places.js
@@ -5,8 +5,27 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-// More code ...
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
 
+// More code ...
+//I don't know where this code is supposed to go, instructions say to "add to POST / Places route.""
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
   
   
 
--- models/places.js
+++ models/places.js
@@ -13,20 +13,5 @@ module.exports = [{
 }]
 
 
-//I don't know where this code is supposed to go, instructinos say to add to POST / Places route.
-router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
+
   
