2a2301732602f817cde4990eea2a2c194edf5d5f
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-29 19:38:56 -0500

    added urlencoded and my for stopped working.

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 
 // GET /places
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
 })
+
 
 router.get('/', (req, res) => {
     let places = [{
