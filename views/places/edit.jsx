9f0c50eb8272afebb8ea015bc5b9a057126f3630
Mason Benzinger <mason.benzinger@gmail.com>
2022-07-31 18:18:07 -0500

    finsihed adding code in part 7. still broke from part 5.6

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,19 +1,41 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('../default.jsx')
 
-function edit (html) {
+function edit_form (data) {
     return (
-        <html>
-            <head>
-                <title>Title</title>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
+        <Def>
+           <main>
+            <h1>Add a New Place</h1>
+            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+  <div className='row'>
+    <div className="form-group col-sm-6">
+        <label htmlFor="name">Place Name</label>
+        <input className="form-control" id="name" name="name" required value={data.pace.name}/>
+    </div>
+    <div className="form-group col-sm-6">
+        <label htmlFor="pic">Place Picture</label>
+        <input className="form-control" id="pic" name="pic" />
+    </div>
+  </div>
+  <div className='row'>
+  <div className="form-group col-sm-6">
+    <label htmlFor="city">City</label>
+    <input className="form-control" id="city" name="city" />
+  </div>
+  <div className="form-group col-sm-6">
+    <label htmlFor="state">State</label>
+    <input className="form-control" id="state" name="state" />
+  </div>
+  </div>
+  <div className="form-group">
+    <label htmlFor="cuisines">Cuisines</label>
+    <input className="form-control" id="cuisines" name="cuisines" required />
+  </div>
+  <input className="btn btn-primary" type="submit" value="Add Place" />
+</form>
+          </main>
+        </Def>
     )
-  }
-  
+}
 
-
-module.exports = edit
+module.exports = edit_form
