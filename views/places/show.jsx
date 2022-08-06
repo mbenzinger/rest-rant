70e2ca84a9f5950f14e3da6d4b3f10d5a8cc8567
Mason Benzinger <mason.benzinger@gmail.com>
2022-08-06 10:37:53 -0500

    added (Data) to show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,30 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>{DataTransfer.place.name}</h1> 
+          <div className="row">
+          <div className="col-sm-6">
+        <img
+            src={data.place.pic}
+            alt={data.place.name}
+            />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        </div>
           </main>
           <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
             Edit
