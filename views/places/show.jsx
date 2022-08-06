e02f105924a7663bfe065eccdb6c616d27fbcae9
Mason Benzinger <mason.benzinger@gmail.com>
2022-08-06 09:11:54 -0500

    working to resolve error on show.jsx line 8. pasted in code up to 9.2.4

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,7 +5,7 @@ function show () {
     return (
         <Def>
           <main>
-            <h1>{DataTransfer.place.name}</h1>
+            <h1>{DataTransfer.place.name}</h1> 
           </main>
           <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
             Edit
@@ -20,3 +20,32 @@ function show () {
 }
 
 module.exports = show
+
+
+/* 9.2.4 copied code here.  cannot determine where thsi goes. Also recieving an error regarding line 8 "data transefer is not defnined"
+<Def>
+  <main>
+    <div className="row">
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+        ...
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+        ...
+    </div>
+  </main>
+</Def>
+
+*/
